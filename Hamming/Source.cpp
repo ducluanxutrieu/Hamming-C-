b3ba10f45db9ec059fc90c2d95ceea5d1ed44ed4
Tran Duc Luan <ducluanxutireu@gmail.com>
2018-12-18 19:11:52 +0700

    Update Cua Co

--- Hamming/Source.cpp
+++ Hamming/Source.cpp
@@ -5,17 +5,18 @@ using namespace std;
 int main() {
 	int data[10];
 	int dataatrec[10], c, c1, c2, c3, i;
+	char temp[3];
 
 	cout << "Enter 4 bits of data one by one\n";
 	cin >> data[0];
 	cin >> data[1];
 	cin >> data[2];
-	cin >> data[4];
+	cin >> data[3];
 
 	//Calculation of even parity
-	data[6] = data[0] ^ data[2] ^ data[4];
-	data[5] = data[0] ^ data[1] ^ data[4];
-	data[3] = data[0] ^ data[1] ^ data[2];
+	data[6] = data[1] ^ data[2] ^ data[3];
+	data[5] = data[0] ^ data[1] ^ data[2];
+	data[4] = data[0] ^ data[2] ^ data[3];
 
 	cout << "\nEncoded data is\n";
 	for (i = 0; i < 7; i++)
@@ -25,11 +26,13 @@ int main() {
 	for (i = 0; i < 7; i++)
 		cin >> dataatrec[i];
 
-	c1 = dataatrec[6] ^ dataatrec[4] ^ dataatrec[2] ^ dataatrec[0];
-	c2 = dataatrec[5] ^ dataatrec[4] ^ dataatrec[1] ^ dataatrec[0];
-	c3 = dataatrec[3] ^ dataatrec[2] ^ dataatrec[1] ^ dataatrec[0];
-	c = c3 * 4 + c2 * 2 + c1;
-
+	c1 = dataatrec[6] ^ dataatrec[3] ^ dataatrec[2] ^ dataatrec[1];
+	c2 = dataatrec[5] ^ dataatrec[2] ^ dataatrec[1] ^ dataatrec[0];
+	c3 = dataatrec[4] ^ dataatrec[3] ^ dataatrec[2] ^ dataatrec[0];
+	 c = c3 + c2 + c1;
+	 temp[0] = c3;
+	 temp[1] = c2;
+	 temp[2] = c1;
 	if (c == 0) {
 		cout << "\nNo error while transmission of data\n";
 	}
@@ -47,10 +50,59 @@ int main() {
 		cout << "\nCorrect message is\n";
 
 		//if errorneous bit is 0 we complement it else vice versa
-		if (dataatrec[7 - c] == 0)
-			dataatrec[7 - c] = 1;
+
+		if (c3 == 0) {
+			if (c2 == 0)
+			{
+				if (c1 == 1)
+				{
+					//001
+					dataatrec[6] = 1 - dataatrec[6];
+				}
+			}
+			else {
+				if (c1 == 0)
+				{
+					//010
+					dataatrec[5] = 1 - dataatrec[5];
+				}
+				else {
+					//011
+					dataatrec[1] = 1 - dataatrec[1];
+				}
+			}
+		}
 		else
-			dataatrec[7 - c] = 0;
+		{
+			if (c2 == 0)
+			{
+				if (c1 == 0)
+				{
+					//100
+					dataatrec[4] = 1 - dataatrec[4];
+				}
+				else {
+					//101
+					dataatrec[3] = 1 - dataatrec[3];
+				}
+			}
+			else {
+				if (c1 == 0)
+				{
+					//110
+					dataatrec[0] = 1 - dataatrec[0];
+				}
+				else {
+					//111
+					dataatrec[2] = 1 - dataatrec[2];
+				}
+			}
+		}
+
+		//if (dataatrec[7 - c] == 0)
+		//	dataatrec[7 - c] = 1;
+		//else
+		//	dataatrec[7 - c] = 0;
 		for (i = 0; i < 7; i++) {
 			cout << dataatrec[i];
 		}
